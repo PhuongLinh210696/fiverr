2ece91da48180908e8b60839858ef6c0559232dd
Chí Công <chicongtkn@gmail.com>
2023-07-29 14:34:26 +0700

    You Need It, We Got It Section

---
+++ src/Components/YouNeedWeGot/YouNeedWeGot.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import './YouNeedWeGot.scss'
+
+const YouNeedWeGot = () => {
+  return (
+    <div className='ynwg-container'>
+        <h2>You need it, we've got it</h2>
+        <ul className='ynwg-list'>
+            <li>
+                <a href='#'>
+                    <img src='./Image/graphics-design.svg' alt=''/>
+                    Graphics & Design
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/online-marketing.svg' alt=''/>
+                    Digital Marketing
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/writing-translation.svg' alt=''/>
+                    Writing & Translation
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/video-animation.svg' alt=''/>
+                    Video & Animation
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/music-audio.svg' alt=''/>
+                    Music & Audio
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/programming.svg' alt=''/>
+                    Programming & Tech
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/business.svg' alt=''/>
+                    Business
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/lifestyle.svg' alt=''/>
+                    Lifestyle
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/data.svg' alt=''/>
+                    Data
+                </a>
+            </li>
+            <li>
+                <a href='#'>
+                    <img src='./Image/photography.svg' alt=''/>
+                    Photography
+                </a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default YouNeedWeGot
