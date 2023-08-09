5b8c614ae692be31958df583e80fa19348d8c005
Chí Công <chicongtkn@gmail.com>
2023-08-09 10:37:53 +0700

    import Categories

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import UserTemplate from "./Templates/UserTemplate";
 import AdminTemplate from "./Templates/AdminTemplate"
 import SignInAdmin from "./Pages/SignInAdmin/SignInAdmin";
 import Services from "./Pages/Services/Services";
+import Categories from "./Components/Categories/Categories";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserTemplate/>}>
           <Route index element={<HomePage/>}/>
+          <Route path="cate" element={<Categories/>}/>
         </Route>
         <Route path="/admin" element={<AdminTemplate/>}>
             <Route path="typeofworks" element={<TypeOfWorks/>}/>
