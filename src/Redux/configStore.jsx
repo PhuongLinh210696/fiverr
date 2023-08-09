699baebbc1d95e8dd19ddae13b12ee50bb1d0636
Chí Công <chicongtkn@gmail.com>
2023-08-09 10:40:05 +0700

    add work

--- src/Redux/configStore.jsx
+++ src/Redux/configStore.jsx
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit'
 import authSlice from './slices/authSlice';
 import towSlice from './slices/towSlice';
 import serSlice from './slices/serSlice';
+import wSlice from './slices/wSlice';
 
 export const store = configureStore({
     reducer:{
-        user:authSlice,
-        typeOfWorks:towSlice,
-        services:serSlice
+        user: authSlice,
+        typeOfWorks: towSlice,
+        services: serSlice,
+        works: wSlice,
     },
 });
