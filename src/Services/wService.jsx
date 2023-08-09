8ad5269a811e630fcdaab535a612d335c5e5d6e4
Chí Công <chicongtkn@gmail.com>
2023-08-09 23:00:52 +0700

    modify error spell check api

--- src/Services/wService.jsx
+++ src/Services/wService.jsx
@@ -5,36 +5,36 @@ export const wService = {
     return https.get("api/cong-viec");
   },
   addW: (data) => {
-    return https.post("api/cong-viec", data)
+    return https.post("api/cong-viec", data);
   },
   getWSearch: () => {
     return https.get("api/cong-viec/phan-trang-tim-kiem");
   },
   getWById: (id) => {
-    return https.get(`api/cong-viec/${id}`)
+    return https.get(`api/cong-viec/${id}`);
   },
   addWId: (data, id) => {
-    return https.put(`api/cong-viec/${id}`, data)
+    return https.put(`api/cong-viec/${id}`, data);
   },
   deleteWId: (id) => {
     return https.delete(`api/cong-viec/${id}`);
   },
   addWImgById: (data, id) => {
-    return https.post(`api/cong-viec/upload-hinh-cong-viec/${id}`, data)
+    return https.post(`api/cong-viec/upload-hinh-cong-viec/${id}`, data);
   },
   getWMenu: () => {
-    return https.get("api/cong-viec/lay-menu-loai-cong-viec")
+    return https.get("api/cong-viec/lay-menu-loai-cong-viec");
   },
   getWMenuByWId: (id) => {
-    return https.get(`api/cong-viec/lay-chi-tiet-loai-cong-viec/${id}`)
+    return https.get(`api/cong-viec/lay-chi-tiet-loai-cong-viec/${id}`);
   },
   getWMenuByWDetail: (id) => {
-    return https.get(`api/cong-viec/lay-cong-viec-theo-chi-tiet-loai/${id}`)
+    return https.get(`api/cong-viec/lay-cong-viec-theo-chi-tiet-loai/${id}`);
   },
-  getWMenu: (id) => {
-    return https.get(`api/cong-viec/lay-cong-viec-chi-tiet/${id}`)
+  getWMenuDById: (id) => {
+    return https.get(`api/cong-viec/lay-cong-viec-chi-tiet/${id}`);
   },
-  getWMenu: (id) => {
-    return https.get(`api/cong-viec/lay-danh-sach-cong-viec-theo-ten/${id}`)
+  getWMenuLByName: (id) => {
+    return https.get(`api/cong-viec/lay-danh-sach-cong-viec-theo-ten/${id}`);
   },
 };
