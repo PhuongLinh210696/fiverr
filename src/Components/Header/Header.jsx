9b56a5a129d796035815b7c8c22addfde2edb07b
Chí Công <chicongtkn@gmail.com>
2023-08-14 22:03:19 +0700

    edit Header to href to userProfile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -235,10 +235,10 @@ const Header = () => {
                           <>
                             <li>
                               <a
-                                href="#"
+                                href={`/UserProfileWork/${userName.user.id}`}
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                               >
-                                Gigs
+                                User Profile
                               </a>
                             </li>
                             <li>
